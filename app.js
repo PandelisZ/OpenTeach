2d49d0173ad0b8dcfef37d1435f8661c132d1e27
PandelisZ <p@ndelis.com>
2015-11-07 18:58:26 +0000

    ejs and bootstrap

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 var pg = require('pg');
 
-pg.connect('postgres://localhost/openteach', function(err, client, done) {
+pg.connect('postgres://openteach:openteach@localhost/openteach', function(err, client, done) {
   client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username VARCHAR NOT NULL UNIQUE, email VARCHAR NOT NULL UNIQUE, password VARCHAR NOT NULL, firstname VARCHAR NOT NULL, lastname VARCHAR NOT NULL);' +
     'CREATE TABLE IF NOT EXISTS skills(id SERIAL PRIMARY KEY, name VARCHAR NOT NULL UNIQUE);' +
     'CREATE TABLE IF NOT EXISTS userskills(id SERIAL, userid INT NOT NULL, skillid INT NOT NULL);',
@@ -12,10 +12,24 @@ pg.connect('postgres://localhost/openteach', function(err, client, done) {
   })
 });
 
+
+app.use(express.static(__dirname + '/public'));
+///SERVER DISPLAY STUFF
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
 app.get('/', function(req, res) {
-  res.send('working');
+    res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
 app.listen(8000, function() {
-  console.log('listening');
+  console.log('listening on port 8000');
 });
