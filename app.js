5bf907ce4b0330a792b5808a533b145da76426e5
PandelisZ <p@ndelis.com>
2015-11-08 04:06:04 +0000

    profile

--- app.js
+++ app.js
@@ -28,10 +28,12 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 pg.connect('postgres://droidpantelas:openteach@openteach.c16qq5m1cpxq.eu-west-1.rds.amazonaws.com/openteach', function(err, client, done) {
+  console.log(err)
   client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username VARCHAR NOT NULL UNIQUE, email VARCHAR NOT NULL UNIQUE, password VARCHAR NOT NULL, firstname VARCHAR NOT NULL, lastname VARCHAR NOT NULL, lat DECIMAL, lng DECIMAL);' +
     'CREATE TABLE IF NOT EXISTS skills(id SERIAL PRIMARY KEY, name VARCHAR NOT NULL UNIQUE);' +
     'CREATE TABLE IF NOT EXISTS userskills(id SERIAL, userid INT NOT NULL, skillid INT NOT NULL);',
   function(err, result) {
+    console.log(err)
     done();
   });
 
@@ -48,35 +50,27 @@ pg.connect('postgres://droidpantelas:openteach@openteach.c16qq5m1cpxq.eu-west-1.
   passport.use('login', new LocalStrategy(
     function(username, password, cb) {
       client.query('SELECT * FROM users WHERE password is NOT NULL AND password = crypt($1, password) AND username = $2', [password, username], function(err, result) {
-        console.log(err);
         if(result.rows.length > 0) {
           return cb(null, result.rows[0]);
         } else {
           return cb(null, false, { message: 'Incorrect username/password.' });
         }
+
+        done();
       });
     }
   ));
 
-  passport.use('register', new LocalStrategy({ passReqToCallback: true },
+  passport.use('register', new LocalStrategy(
     function(req, username, password, cb) {
-      console.log(req.body.lastname);
-      client.query('INSERT INTO users(username, email, password, firstname, lastname, lat, lng) VALUES ($1, $2, crypt($3, gen_salt(\'bf\')), $4, $5, $6::DECIMAL, $7::DECIMAL)', [username, req.body.email, password, req.body.firstname, req.body.lastname, req.body.lat, req.body.lng], function(err, result) {
-        done();
-
+      client.query('INSERT INTO users(username, email, password, firstname, lastname, lat, lng) VALUES ($1, $2, crypt($3, gen_salt(\'bf\')), $4, $5, $6, $7)', [username, req.param('email'), password, req.param('firstname'), req.param('lastname'), req.param('lat'), req.param('lng')], function(err, result) {
         if(!err) {
-          console.log('success');
-          client.query('SELECT * FROM users WHERE username = $1', [username], function(err, result) {
-            done();
-
-            console.log(result.rows[0]);
-
-            return cb(null, result.rows[0]);
-          });
+          return cb(null, result.rows[0]);
         } else {
-          console.log(err);
-          return cb(null, false, { message: 'Error making user.' });
+          return done(null, false, { message: 'Error making user.' });
         }
+
+        done();
       });
     }
   ));
@@ -88,16 +82,10 @@ pg.connect('postgres://droidpantelas:openteach@openteach.c16qq5m1cpxq.eu-west-1.
   }
 
   app.get('/login', function(req, res) {
-    res.render('pages/login');
+    //
   });
 
-  app.post('/login', passport.authenticate('login', { successRedirect: '/', failureRedirect: '/login' }));
-
-  app.get('/register', function(req, res) {
-    res.render('pages/register');
-  });
-
-  app.post('/register', passport.authenticate('register', { successRedirect: '/', failureRedirect: '/register' }));
+  app.post('/login', passport.authenticate('login', { successRedirect: '/', failureRedirect: '/login', failureFlash: true }));
 
   app.post('/points', isAuthenticated, function(req, res) {
     client.query('SELECT * FROM users WHERE lat >= $1::decimal AND lat <= $2::decimal AND lng >= $3::decimal AND lng <= $4::decimal', [req.params.latmin, req.params.latmax, req.params.lngmin, req.params.lngmax], function(err, result) {
@@ -111,6 +99,9 @@ pg.connect('postgres://droidpantelas:openteach@openteach.c16qq5m1cpxq.eu-west-1.
       res.render('pages/index');
   });
 
+  app.get('/', function(req, res) {
+      res.render('pages/profile');
+  });
   // about page
   app.get('/about', function(req, res) {
       res.render('pages/about');
@@ -120,15 +111,14 @@ pg.connect('postgres://droidpantelas:openteach@openteach.c16qq5m1cpxq.eu-west-1.
   app.get('/skills', isAuthenticated, function(req, res) {
       res.render('pages/skills');
   });
-
   //var appEnv = cfenv.getAppEnv();
-
+});
   var server_port = process.env.VCAP_APP_PORT || 8000;
   var server_host = process.env.VCAP_APP_HOST || "localhost";
 
   // start server on the specified port and binding host
+
   app.listen(server_port, server_host, function() {
   	// print a message when the server starts listening
     //console.log("server starting on " + server_port);
   });
-});
